8571575f4f2174a9d669885a1b065469214590e9
Bodhana79 <131342893+Bodhana79@users.noreply.github.com>
2023-06-04 08:55:34 +0530

    Add files via upload

---
+++ StrFormate.java
@@ -0,0 +1,11 @@
+public class StrFormate
+{
+  /*Driver code*/
+  public static void main(String args[])
+  {
+    String s1=new String("Hello");
+    String s2=new String("World");
+    String s=String.Formate("%s%s",s1s2);
+   System.out.println(s.toString());
+   }              
+}
---
+++ Teststringcomparision4.java
@@ -0,0 +1,11 @@
+class Teststringcomparision4
+{
+public static void main(String args[])
+{
+String s1="Sachin";
+String s2="Sachin";
+String s3="Ratan";
+System.out.println(s1.compare(s2));//
+System.out.println(s1.compare(s3));//
+System.out.println(s3.compare(s4));//
+}}
